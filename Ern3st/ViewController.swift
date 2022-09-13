cea74495e7d85596cd07a8123387d24d569c267c
Ali <alibwp@live.com>
2022-09-13 15:35:18 +0500

    Initial Commit

---
+++ Ern3st/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ern3st
+//
+//  Created by Muhammad Ali on 13/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
